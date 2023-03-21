7a043470a49361a6983b7709738c10f3bfe9fa17
ROCKET228 <michkahap@gmail.com>
2023-03-21 12:48:19 +0200

    Chess prototype

---
+++ src/modules/figures/Figure.ts
@@ -0,0 +1,45 @@
+import {Colors} from "../Colors";
+import {Cell} from "../Cell";
+// @ts-ignore
+import logo from '../../assets/black-bishop.png'
+
+export enum  FigureNames{
+    FIGURE = '',
+    KING = "King",
+    KNIGHT = "Knight",
+    PAWN = 'Pawn',
+    QUEEN = 'Queen',
+    ROOK = 'Rook',
+    BISHOP = 'Bishop'
+}
+
+export class Figure{
+    color: Colors;
+    logo: typeof  logo | null;
+    cell: Cell;
+    name: FigureNames;
+    id: number;
+
+
+    constructor(color: Colors, cell: Cell) {
+        this.color = color;
+        this.cell = cell;
+        this.cell.figure = this;
+        this.logo = null;
+        this.name = FigureNames.FIGURE
+        this.id = Math.random()
+    }
+
+    canMove(target: Cell) : boolean {
+        if (target.figure?.color === this.color){
+            return false
+        }
+        if(target.figure?.name === FigureNames.KING){
+            return false
+        }
+        return true
+    }
+
+    moveFigure(target: Cell){}
+
+}
